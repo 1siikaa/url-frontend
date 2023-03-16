4ef26f24fe5aa8e46af20248604b27cff3c0296b
1siikaa <srishtidwivedi672@gmail.com>
2023-03-16 14:59:01 +0530

    done

--- src/components/PostUrl/PostUrl.jsx
+++ src/components/PostUrl/PostUrl.jsx
@@ -1,73 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import './PostUrl.css'
-import { CopyToClipboard } from 'react-copy-to-clipboard'
+import React, { useEffect, useState } from 'react';
+import './PostUrl.css';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
-const PostUrl = ({ inputValue }) => {
-    const [shorturl, setShorturl] = useState("")
-    const [copied, setCopied] = useState(false)
-    const [loading, setLoading] = useState(false)
-    const [error, setError] = useState(false)
+const PostUrl = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [shorturl, setShorturl] = useState('');
+  const [copied, setCopied] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-    const fetchData = async (longUrl) => {
-        try {
-            setLoading(true)
-            const res = await fetch("https://vercel.com/1siikaa/url-shortner-7jc3/url/shorten", {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ longUrl })
-            })
-            const result = await res.json()
-            console.log("shortUrl", result.data.shortUrl)
-            setShorturl(result.data.shortUrl)
-            setLoading(false)
-        }
-        catch (err) {
-            setError(err)
-        }
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(`https://short-api-51t8.onrender.com/url/shorten`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ longUrl: inputValue })
+      });
+      const result = await res.json();
+      console.log('shortUrl', result.data.shortUrl);
+      setShorturl(result.data.shortUrl);
+      setLoading(false);
+    } catch (err) {
+      setError(err);
     }
+  };
 
-    useEffect(() => {
-        if (inputValue.length) {
-            fetchData(inputValue)
-        }
-    }, [inputValue])
-
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setCopied(false)
-        }, 1000)
-
-        return () => clearTimeout(timer)
-    }, [copied])
-
-    if (loading) {
-        return <p className='noData' >Loading...</p>
+  useEffect(() => {
+    if (inputValue.length) {
+      fetchData();
     }
+  }, [inputValue]);
 
-    if (error) {
-        return <p className='noData' >Something went wrong</p>
-    }
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCopied(false);
+    }, 1000);
 
-    return (
-        <>
-            {shorturl && (
-                <div className='result'>
-                    <p>{shorturl}</p>
+    return () => clearTimeout(timer);
+  }, [copied]);
 
-                    <CopyToClipboard
-                        text={shorturl}
-                        onCopy={() => setCopied(true)}
-                    >
-                        <button className={copied ? "copied" : ""}>
-                            <i className="fa fa-clone"></i>Copy
-                        </button>
-                    </CopyToClipboard>
-                </div>
-            )}
-        </>
-    )
-}
+  if (loading) {
+    return <p className='noData'>Loading...</p>;
+  }
 
-export default PostUrl
+  if (error) {
+    return <p className='noData'>Something went wrong</p>;
+  }
+
+  return (
+    <div className='link-result-container'>
+      <input
+        type='text'
+        value={inputValue}
+        placeholder='Paste long URL and shorten it'
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      {shorturl && (
+        <div className='result'>
+          <p>{shorturl}</p>
+
+          <CopyToClipboard text={shorturl} onCopy={() => setCopied(true)}>
+            <button className={copied ? 'copied' : ''}>
+              <i className='fa fa-clone'></i>Copy
+            </button>
+          </CopyToClipboard>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default PostUrl;
+
